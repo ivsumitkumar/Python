f4ad92c7f9d65a7617114fec3b3097b33a9bc45f
Sumit Kumar <vishnusumit2@gmail.com>
2022-04-17 18:52:14 +0530

    GFG POTD SOLVED 17/04/2022

---
+++ GFG-POTD/0091_(UNSOLVED)_closest_palindrome.py
@@ -0,0 +1,41 @@
+'''
+LEVEL:>>> Medium
+Given a number num, our task is to find the closest Palindrome number whose absolute difference with given number is minimum. If 2 Palindome numbers have same absolute difference from the given number, then find the smaller one.
+ 
+
+Example 1:
+
+Input: num = 9
+Output: 9
+Explanation: 9 itself is a palindrome
+number.
+Example 2:
+
+Input: num = 489
+Output: 494
+Expnataion: closest palindrome numbers from
+489 are 484 and 494. Absolute difference between
+489 and 494 is equal to the absolute difference
+between 484 and 489 but 484 is smaller than 494.
+ 
+
+Your Task:
+You don't need to read or print anyhting. Your task is to complete the function closestPalindrome() which takes num as input parameter and returns the closest palindrome.
+ 
+
+Expected Time Complexity: O(log10num)
+Expected Space Complexity: O(1)
+ 
+
+Constraints:
+1 <= num <= 1014
+
+Related Topics:>>>
+Mathematical
+
+SAMPLE INPUT:>>>
+1
+9
+OUTPUT:>>>
+9
+'''
